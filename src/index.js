e640a17d77eb9f62fff617d6759e0821cdc9cdd3
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-01 01:40:07 +0530

    multiple inline styles

--- src/index.js
+++ src/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class Hello extends React.Component{
-    render(){
-        return <div><h1>Hello World</h1></div>;
-    }
+const allStyles = {
+  color: "red",
+  background: "grey"
+};
+
+class Hello extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1 style={allStyles}>Hello World</h1>
+      </div>
+    );
+  }
 }
 
-
-
-
-
 ReactDOM.render(<Hello />, document.getElementById("root"));
-
