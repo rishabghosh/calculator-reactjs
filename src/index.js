97154b697d90f43f52929f48bcf6ed9424eb8f54
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-01 12:02:36 +0530

    implement first cut of view of the calculator

--- src/index.js
+++ src/index.js
@@ -25,17 +25,36 @@ class Buttons extends React.Component {
   render() {
     return (
       <div className="buttons">
-        <ButtonRow />
+        <ButtonRow symbols={symbols.row1}/>
+        <ButtonRow symbols={symbols.row2}/>
+        <ButtonRow symbols={symbols.row3}/>
+        <ButtonRow symbols={symbols.row4}/>
       </div>
     );
   }
 }
 
+const symbols = {
+    row1: ["1", "2", "3", "+"],
+    row2: ["4", "5", "6", "-"],
+    row3: ["7", "8", "9", "*"],
+    row4: ["0", ".", "=", "/"]
+};
+
 class ButtonRow extends React.Component {
+    constructor(props){
+        super(props);
+        this.symbols = props.symbols;
+    }
+
   render() {
     return (
       <div className="button-row">
-        <Button symbol="1" />
+        {/* <Button symbol="1" />
+        <Button symbol="2" />
+        <Button symbol="3" />
+        <Button symbol="+" /> */}
+        {this.symbols.map(symbol=> <Button symbol={symbol}/>)}
       </div>
     );
   }
@@ -46,7 +65,6 @@ class Button extends React.Component {
     super(props);
     this.symbol = props.symbol;
   }
-
   render() {
     return <button>{this.symbol}</button>;
   }
