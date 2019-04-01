0cd71697e2b29d10900c97481c10cf84e02b3804
rishabghosh <44020133+rishabghosh@users.noreply.github.com>
2019-04-01 11:26:28 +0530

    Implement components - Box, display, Buttons, ButtonRow, Button

--- src/index.js
+++ src/index.js
@@ -2,19 +2,54 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const allStyles = {
-  color: "red",
-  background: "grey"
-};
-
-class Hello extends React.Component {
+class Box extends React.Component {
   render() {
     return (
-      <div>
-        <h1 style={allStyles}>Hello World</h1>
+      <main>
+        <div className="box">
+          <Display />
+          <Buttons />
+        </div>
+      </main>
+    );
+  }
+}
+
+class Display extends React.Component {
+  render() {
+    return <div className="display" />;
+  }
+}
+
+class Buttons extends React.Component {
+  render() {
+    return (
+      <div className="buttons">
+        <ButtonRow />
       </div>
     );
   }
 }
 
-ReactDOM.render(<Hello />, document.getElementById("root"));
+class ButtonRow extends React.Component {
+  render() {
+    return (
+      <div className="button-row">
+        <Button symbol="1" />
+      </div>
+    );
+  }
+}
+
+class Button extends React.Component {
+  constructor(props) {
+    super(props);
+    this.symbol = props.symbol;
+  }
+
+  render() {
+    return <button>{this.symbol}</button>;
+  }
+}
+
+ReactDOM.render(<Box />, document.getElementById("root"));
